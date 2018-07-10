d37a6e610e82aa434b363b7eabb7dab1d20be2eb
Kusumanjali <kusumanjalikorlapati@gmail.com>
2018-07-10 14:17:11 +0530

    changes for the forgot password

--- HR.Web/Controllers/AccountController.cs
+++ HR.Web/Controllers/AccountController.cs
@@ -7,19 +7,23 @@ using System.Web.Mvc;
 using System.Web.Security;
 using HR.Web.Helpers;
 using HR.Web.BusinessObjects.Security;
+using HR.Web.BusinessObjects.Operation;
 namespace HR.Web.Controllers
 {
     
     public class AccountController : BaseController
     {
-        // GET: Account
+		// GET: Account
 
-        UserBO userBo = null;
-        public AccountController()
-        {
-            userBo = new UserBO(SESSIONOBJ);
-        }
-        public ActionResult Login()
+		UserBO userBo = null;
+		EmployeeHeaderBO empHeaderBO = null;
+		public AccountController()
+		{
+			userBo = new UserBO(SESSIONOBJ);
+			empHeaderBO = new EmployeeHeaderBO(SESSIONOBJ);
+		}
+
+		public ActionResult Login()
         {
             return View();
         }
@@ -118,5 +122,72 @@ namespace HR.Web.Controllers
             return Json(new { success });
             // return RedirectToAction("Login");
         }
-    }
+		[HttpGet]
+		public ActionResult ForgotPassword(string emailID)
+		{
+
+
+			var strbody = string.Empty;
+			var subject = "Reset Password - HR";
+			User userobj = userBo.GetByProperty(x => x.UserName.ToUpper() == emailID.ToUpper());
+			var employeeid = userobj.EmployeeId;
+			if (employeeid == -1)
+			{
+
+				var newPassword = UTILITY.CreateRandomPassword();
+				userobj.Password = newPassword;
+				strbody =
+					string.Format(
+					"Dear Admin <BR>" +
+					"As you requested, your password for HR login has now been reset. Your new login details are as follows: <BR>" +
+					"Email ID :{0} <BR>" +
+					"Password : {1} <BR>" +
+					"To change your password to something more memorable, after logging in go to My Profile, Change Password.<BR>" +
+					"<BR>" +
+					"Regards<BR>" +
+					"Administrator<BR>" +
+					"EZY-CORP<BR>",
+					emailID,
+					newPassword);
+			}
+			else
+			{
+				EmployeeHeader empobj = empHeaderBO.GetByProperty(x => x.EmployeeId == employeeid);
+
+				var newPassword = UTILITY.CreateRandomPassword();
+				empobj.Password = newPassword;
+				userobj.Password = newPassword;
+				strbody =
+					string.Format(
+					"Dear {0} {1} <BR>" +
+					"As you requested, your password for HR login has now been reset. Your new login details are as follows: <BR>" +
+					"Email ID :{2} <BR>" +
+					"Password : {3} <BR>" +
+					"To change your password to something more memorable, after logging in go to My Profile, Change Password.<BR>" +
+					"<BR>" +
+					"Regards<BR>" +
+					"Administrator<BR>" +
+					"EZY-CORP<BR>",
+					empobj.FirstName,
+					empobj.LastName,
+					emailID,
+					newPassword);
+				empHeaderBO.Add(empobj);
+			}
+
+			//}
+			/*
+             "From:" + empleavelist.FromDate.ToShortDateString() + "to"  + empleavelist.ToDate.ToShortDateString() + "<BR>" 
+                + "Reason:" + empleavelist.Reason;
+             */
+			userBo.Add(userobj);
+
+			EmailGenerator emailgenerator = new EmailGenerator();
+			emailgenerator.ConfigMail(emailID, true, subject, strbody);
+
+
+			var success = "Your new password is sent to your login email ID : " + emailID;
+			return Json(new { success }, JsonRequestBehavior.AllowGet);
+		}
+	}
 }
